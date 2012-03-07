a26f337dd3ea3b020ecb12403ccad49304de476a
Ferran Basora <fcsonline@gmail.com>
2012-03-07 23:22:59 +0100

    Better canvas, JSON, lineal draw

--- app.js
+++ app.js
@@ -2,9 +2,6 @@ var app = require('express').createServer()
 var io = require('socket.io').listen(app);
 var path = require('path');
 
-app.listen('8081');
-
-
 if (process.argv[2] != undefined){
    filename = process.argv[2];
    if (!path.existsSync(filename)){
@@ -17,13 +14,26 @@ if (process.argv[2] != undefined){
 app.get('/', function(req, res) {
   res.sendfile(__dirname + '/index.html');
 });
+
 app.get('/reqscanvas.js', function(req, res) {
   res.sendfile(__dirname + '/reqscanvas.js');
 });
+
+app.get('/json2.js', function(req, res) {
+  res.sendfile(__dirname + '/json2.js');
+});
+
+console.info('Server running at http://127.0.0.1:8081/ and following "' + filename + '"');
+app.listen('8081');
+
 
 var spawn = require('child_process').spawn;
 var tail = spawn('tail', ['-f', filename]);
 
 tail.stdout.on('data', function (data) {
-  io.sockets.emit('log', data.toString('utf8'));
+  var str = data.toString('utf8');
+  var regexp = /([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+).+\[(.+)\] /g;
+  var match = regexp.exec(str);
+  var robj = {ip: match[1], time: match[2]};
+  io.sockets.emit('log', JSON.stringify(robj));
 });
--- reqscanvas.js
+++ reqscanvas.js
@@ -1,210 +1,165 @@
 /**
-*   Liquid particles canvas experiment
-*   ©2010 spielzeugz.de 
-*/
+ *   Liquid particles canvas experiment
+ *   ©2010 spielzeugz.de 
+ */
 (function(){
-	
-	var PI_2        = Math.PI * 2;
-	
-	var canvasW     = 1000;
-	var canvasH     = 560;
-	var friction    = 0.96;
-	var requests      = [];
-	
-	var canvas;
-	var ctx;
-	var canvasDiv;
-	var outerDiv;
-	
-	var mouseX;
-	var mouseY;
-	var mouseVX;
-	var mouseVY;
-	var prevMouseX;
-	var prevMouseY;
-	var isMouseDown;
 
+ var PI_2        = Math.PI * 2;
 
-	function init(){
-		canvas = document.getElementById("mainCanvas");
-		
-		if ( canvas.getContext ){
-			setup();
-			setInterval( run , 33 );
-		}
-		else{
-			alert("Sorry, needs a recent version of Chrome, Firefox, Opera, Safari, or Internet Explorer 9.");
-		}
-	}
-	   
-	function setup(){
-		outerDiv  = document.getElementById("outer");
-		canvasDiv = document.getElementById("canvasContainer");
-		ctx       = canvas.getContext("2d");
-		
-		mouseX = prevMouseX = canvasW * 0.5;
-		mouseY = prevMouseY = canvasH * 0.5;
-		
-		document.onmousedown = onDocMouseDown;
-		document.onmouseup   = onDocMouseUp;
-		document.onmousemove = onDocMouseMove;
-	}
+ var canvasW     = 1000;
+ var canvasH     = 560;
+ var friction    = 0.99;
+ var requests    = [];
+ var total       = 0;
 
-	function run(){
-		ctx.globalCompositeOperation = "source-over";
-		ctx.fillStyle = "rgba(8,8,12,0.65)";
-		ctx.fillRect( 0 , 0 , canvasW , canvasH );
-		ctx.globalCompositeOperation = "lighter";
-		
-		mouseVX    = mouseX - prevMouseX;
-		mouseVY    = mouseY - prevMouseY;
-		prevMouseX = mouseX;
-		prevMouseY = mouseY;
-		
-		var toDist   = canvasW * 0.86;
-		var stirDist = canvasW * 0.125;
-		var blowDist = canvasW * 0.5;
-		
-		var Mrnd = Math.random;
-		var Mabs = Math.abs;
-		
-		var i = requests.length;
-		while ( i-- ){
-			var m  = requests[i];
-			var x  = m.x;
-			var y  = m.y;
-			var vX = m.vX;
-			var vY = m.vY;
-			
-			var dX = x - mouseX;
-			var dY = y - mouseY; 
-			var d  = Math.sqrt( dX * dX + dY * dY ) || 0.001;
-			dX /= d;
-			dY /= d;
-			
-			if ( isMouseDown ){
-				if ( d < blowDist ){
-					var blowAcc = ( 1 - ( d / blowDist ) ) * 14;
-					vX += dX * blowAcc + 0.5 - Mrnd();
-					vY += dY * blowAcc + 0.5 - Mrnd();
-				}
-			}
-			
-			if ( d < toDist ){
-				var toAcc = ( 1 - ( d / toDist ) ) * canvasW * 0.0014;
-				vX -= dX * toAcc;
-				vY -= dY * toAcc;			
-			}
-			
-			if ( d < stirDist ){
-				var mAcc = ( 1 - ( d / stirDist ) ) * canvasW * 0.00026;
-				vX += mouseVX * mAcc;
-				vY += mouseVY * mAcc;			
-			}
-			
-			vX *= friction;
-			vY *= friction;
-			
-			var avgVX = Mabs( vX );
-			var avgVY = Mabs( vY );
-			var avgV  = ( avgVX + avgVY ) * 0.5;
-			
-			if( avgVX < .1 ) vX *= Mrnd() * 3;
-			if( avgVY < .1 ) vY *= Mrnd() * 3;
-			
-			var sc = avgV * 0.45;
-			sc = Math.max( Math.min( sc , 3.5 ) , 0.4 );
-			
-			var nextX = x + vX;
-			var nextY = y + vY;
-			
-			if ( nextX > canvasW ){
-				nextX = canvasW;
-				vX *= -1;
-			}
-			else if ( nextX < 0 ){
-				nextX = 0;
-				vX *= -1;
-			}
-			
-			if ( nextY > canvasH ){
-				nextY = canvasH;
-				vY *= -1;
-			}
-			else if ( nextY < 0 ){
-				nextY = 0;
-				vY *= -1;
-			}
-			
-			m.vX = vX;
-			m.vY = vY;
-			m.x  = nextX;
-			m.y  = nextY;
-			
-			ctx.fillStyle = m.color;
-			ctx.beginPath();
-			ctx.arc( nextX , nextY , sc , 0 , PI_2 , true );
-			ctx.closePath();
-			ctx.fill();		
-		}
-	}
+ var canvas;
+ var ctx;
+ var canvasDiv;
 
+ function init(){
+   canvas = $("#mainCanvas")[0];
 
-	function onDocMouseMove( e ){
-		var ev = e ? e : window.event;
-		mouseX = ev.clientX - outerDiv.offsetLeft - canvasDiv.offsetLeft;
-		mouseY = ev.clientY - outerDiv.offsetTop  - canvasDiv.offsetTop;
-	}
+   if ( canvas.getContext ){
+     setup();
+     setInterval( run , 33 );
+   }
+   else{
+     alert("Sorry, needs a recent version of Chrome, Firefox, Opera, Safari, or Internet Explorer 9.");
+   }
+ }
 
-	function onDocMouseDown( e ){
-		isMouseDown = true;
-		return false;
-	}
+ function setup(){
+   canvasDiv = $("#canvasContainer")[0];
 
-	function onDocMouseUp( e ){
-		isMouseDown = false;
-		return false;
-	}
+   canvasW = canvasDiv.offsetWidth;
+   canvasH = canvasDiv.offsetHeight;
 
+   canvas.setAttribute("width", canvasW);
+   canvas.setAttribute("height", canvasH);
 
-	function RemoteRequest(){
-		this.color = "rgb(" + Math.floor( Math.random()*255 ) + "," + Math.floor( Math.random()*255 ) + "," + Math.floor( Math.random()*255 ) + ")";
-		this.y     = 0;
-		this.x     = 0;
-		this.vX    = 0;
-		this.vY    = 0;
-		this.size  = 1; 
-	}
+   console.log ("Initialize canvas with size: " + canvasW + "x" + canvasH);
 
+   ctx = canvas.getContext("2d");
+ }
 
-	function rect( context , x , y , w , h ){
-		context.beginPath();
-		context.rect( x , y , w , h );
-		context.closePath();
-		context.fill();
-	}
+ function run(){
+   ctx.globalCompositeOperation = "source-over";
+   ctx.fillStyle = "rgba(8,8,12,0.65)";
+   ctx.fillStyle = "rgb(0,0,0)";
+   ctx.fillRect( 0 , 0 , canvasW , canvasH );
+   ctx.globalCompositeOperation = "lighter";
 
+   var Mrnd = Math.random;
+   var Mabs = Math.abs;
 
-	function trace( str ){
-		document.getElementById("output").innerHTML = str;
-	}
+   var orequests = [];
+   var i = requests.length;
+   while ( i-- ){
+     var m  = requests[i];
+     var x  = m.x;
+     var y  = m.y;
+     var vX = m.vX;
+     var vY = m.vY;
 
+     var avgVX = Mabs( vX );
+     var avgVY = Mabs( vY );
+     var avgV  = ( avgVX + avgVY ) * 0.5;
 
-	window.onload = init;
+     if( avgVX < .1 ) vX *= Mrnd() * 3;
+     if( avgVY < .1 ) vY *= Mrnd() * 3;
 
-  // Establish the websocket connection
-  var socket = io.connect('localhost', {port:8081});
-  socket.on('log', function (data) {
-    // $('#log').append('<br />' + data);
-    // console.log(data);
+     var sc = avgV * 0.45;
+     sc = Math.max( Math.min( sc , 4.5 ) , 0.4 );
 
-    var i = requests.length;
-    var m = new RemoteRequest();
-    m.x   = canvasW * 0.5;
-    m.y   = canvasH * 0.5;
-    m.vX  = Math.cos(i) * Math.random() * 34;
-    m.vY  = Math.sin(i) * Math.random() * 34;
-    requests.push(m);
-  });
-	
+     sc = m.size;
+
+     var nextX = x + vX;
+     var nextY = y + vY;
+
+     if ( nextX > canvasW ){
+       nextX = canvasW;
+       vX *= -1;
+     } else if ( nextX < 0 ){
+       orequests.push(i);
+       // nextX = 0;
+       // vX *= -1;
+     }
+
+     if ( nextY > canvasH ){
+       nextY = canvasH;
+       vY *= -1;
+     } else if ( nextY < 0 ){
+       nextY = 0;
+       vY *= -1;
+     }
+
+     m.vX = vX;
+     m.vY = vY;
+     m.x  = nextX;
+     m.y  = nextY;
+
+     ctx.fillStyle = m.color;
+     ctx.beginPath();
+     ctx.arc( nextX , nextY , sc , 0 , PI_2 , true );
+     ctx.closePath();
+     ctx.fill();
+   }
+
+    var x = canvasW - 60;
+    var y = canvasH - 5;
+    ctx.font = "10pt Arial";
+    ctx.fillStyle = "#ffffff"; // text color
+    ctx.fillText(pad(total, 8), x, y);
+
+   // Remove obsolete requests
+   requests = $.grep(requests, function(n, i){
+      return $.inArray(i, orequests);
+   });
+
+ }
+
+ function RemoteRequest(){
+   this.color = "rgb(" + Math.floor( Math.random()*155 + 100 ) + "," + Math.floor( Math.random()*155 + 100 ) + "," + Math.floor( Math.random()*155 + 100 ) + ")";
+   this.x     = 0;
+   this.y     = 0;
+   this.vX    = 0;
+   this.vY    = 0;
+   this.size  = 5;
+ }
+
+ function rect( context , x , y , w , h ){
+   context.beginPath();
+   context.rect( x , y , w , h );
+   context.closePath();
+   context.fill();
+ }
+
+ function pad(num, length) {
+   var str = '' + num;
+   while (str.length < length) {
+     str = '0' + str;
+   }
+
+   return str;
+ }
+
+ window.onload = init;
+
+ // Establish the websocket connection
+ var socket = io.connect('localhost', {port:8081});
+ socket.on('log', function (data) {
+     var robj = JSON.parse(data);
+     console.log(robj);
+
+     var i = requests.length;
+     var m = new RemoteRequest();
+     m.x   = 0; // canvasW * 0.5;
+     m.y   = Math.floor( Math.random() * (canvasH - 30) + 30 ); // canvasH * 0.5;
+     m.vX  = Math.random() * 50 + 10;
+     m.vY  = 0; //Math.sin(i) * Math.random() * 34;
+     requests.push(m);
+
+     ++total;
+ });
+
 })();
