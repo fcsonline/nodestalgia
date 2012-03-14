665f1cd3dcb48c08136d03f3889de348ca1fa864
Ferran Basora <fcsonline@gmail.com>
2012-03-14 15:29:27 +0100

    Restore DNS

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
+var sys = require('util');
+var dns = require('dns');
+var events = require('events');
+
 var app = require('express').createServer()
 var io = require('socket.io').listen(app);
 var path = require('path');
 
-// print process.argv
+/* print process.argv
 process.argv.forEach(function (val, index, array) {
   console.log(index + ': ' + val);
-  });
+});
+*/
 
 var filename = new Array();
 if (process.argv.length > 2){
@@ -41,12 +46,50 @@ var tail = spawn('tail', ['-f'].concat(filename));
 // 127.0.0.1 - - [07/Mar/2012:23:21:47 +0100] "GET / HTTP/1.0" 200 454 "-" "ApacheBench/2.3"
 var regexp = /([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+).+\[(.+)\] "(\w+) ([^ ]+) .*" (\w+) (\w+)/g;
 
+// Hashmap for DNS resolves
+var hmdns = {};
+
 tail.stdout.on('data', function (data) {
   var str = data.toString('utf8');
   var match = regexp.exec(str);
 
   if (match !== null) {
     var robj = {ip: match[1], time: match[2], method: match[3], path: match[4], result: match[5], size: match[6]};
-    io.sockets.emit('log', JSON.stringify(robj));
+
+    if (hmdns[robj.ip] !== undefined) {
+      robj.ip = hmdns[robj.ip];
+      io.sockets.emit('log', JSON.stringify(robj));
+    } else {
+      reverse_addr(robj.ip).addListener('error', function (addr, err) {
+        console.log(addr + ' failed: ' + err.message);
+      }).addListener('response', function(addr, domains) {
+        if (domains.length === 0) {
+          hmdns[robj.ip] = robj.ip;
+        } else {
+          console.log('DNS: ' + addr + ' resolved to ' + domains[0]);
+          hmdns[robj.ip] = domains[0];
+        }
+
+        // Same that known host
+        robj.ip = hmdns[robj.ip];
+        io.sockets.emit('log', JSON.stringify(robj));
+      });
+    }
+
   }
 });
+
+// For DNS resolve
+function reverse_addr(addr) {
+    var e = new events.EventEmitter();
+    dns.reverse(addr, function(err, domains) {
+        if (err) {
+            if (err.errno == dns.NOTFOUND)
+                e.emit('response', addr, 'NOTFOUND');
+            else
+                e.emit('error', addr, err);
+        } else
+            e.emit('response', addr, domains);
+    });
+    return e;
+}
