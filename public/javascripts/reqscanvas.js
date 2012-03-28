9a33311e39cf1143fb220219dc00bb403bd479c4
Ferran Basora <fcsonline@gmail.com>
2012-03-28 17:15:47 +0200

    Clean and right margin for target labels

--- public/javascripts/reqscanvas.js
+++ public/javascripts/reqscanvas.js
@@ -7,6 +7,7 @@
  var PI_2        = Math.PI * 2;
  var MAX_MSG_TTL = 50;
  var MARGIN_LEFT = 150;
+ var MARGIN_RIGHT = 150;
  var MARGIN_TOP     = 50;
  var MARGIN_BOTTOM  = 50;
  var DEFAULT_FONT= "10pt Arial";
@@ -103,24 +104,13 @@
      var y  = m.y;
      var vX = m.vX;
      var vY = m.vY;
-
-      // var avgVX = Mabs( vX );
-      // var avgVY = Mabs( vY );
-      // var avgV  = ( avgVX + avgVY ) * 0.5;
-
-      // if( avgVX < .1 ) vX *= Mrnd() * 3;
-      // if( avgVY < .1 ) vY *= Mrnd() * 3;
-
-      // var sc = avgV * 0.45;
-      // sc = Math.max( Math.min( sc , 4.5 ) , 0.4 );
-
      var sc = m.size;
 
      var nextX = x + vX;
      var nextY = y + vY;
 
-     if ( nextX > canvasW ){
-       nextX = canvasW;
+     if ( nextX > canvasW - MARGIN_RIGHT ){
+       nextX = canvasW - MARGIN_RIGHT;
        vX *= -1;
 
        // Push a new message
