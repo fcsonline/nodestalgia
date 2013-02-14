bb23fe1ad4b6504b11372c10af16273fe33d7f5e
Ferran Basora <fcsonline@gmail.com>
2013-02-14 02:03:38 +0100

    jslint

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,13 @@
  * GET home page.
  */
 
-exports.setup = function(app, program) {
-  app.get('/', function(req, res) {
-    res.render('index.jade', { 
-      speed: program.speed, 
-      framerate: program.framerate, 
-      colorize: '' + program.colorize, 
-      sumarize: '' + program.sumarize, 
+exports.setup = function (app, program) {
+  app.get('/', function (req, res) {
+    res.render('index.jade', {
+      speed: program.speed,
+      framerate: program.framerate,
+      colorize: '' + program.colorize,
+      sumarize: '' + program.sumarize,
       time: '' + program.time
     });
   });
